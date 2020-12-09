ee63d2022f26503493d13a92c92895de3bc53ae4
Ian Bruns <ian@rbruns.com>
2020-12-09 10:43:46 -0700

    context given value

--- src/NotefulApp/NotefulApp.js
+++ src/NotefulApp/NotefulApp.js
@@ -6,6 +6,7 @@ import SideBarNote from '../SideBarNote/SideBarNote';
 import NoteStorage from '../NoteStorage/NoteStorage';
 import NoteStorageFolder from '../NoteStorageFolder/NoteStorageFolder';
 import NoteStorageNote from '../NoteStorageNote/NoteStorageNote';
+import NotefulContext from '../NotefulContext'
 import { Route } from 'react-router-dom'
 
 class NotefulApp extends React.Component {
@@ -73,38 +74,45 @@ class NotefulApp extends React.Component {
     }
 
     render() {
+        const contextValue = {
+            notes: this.state.notes,
+            folders: this.state.folders
+        }
         return (
             <div className='NotefulApp'>
-                <div className='flex-one'>
-                    <Route exact path='/'
-                        render={() => <SideBar folders={this.state.folders} />}
-                    />
-                    <Route path='/folder/:folderId'
-                        render={() => <SideBarFolder folders={this.state.folders} />}
-                    />
-                    <Route path='/note/:noteId'
-                        render={(props) => <SideBarNote {...props}
-                            notes={this.state.notes}
-                            folders={this.state.folders} />}
-                    />
-                </div>
-                <div className='flex-three'>
-                    <Route exact path='/'
-                        render={() => <NoteStorage notes={this.state.notes}
-                            deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
-                    />
-                    <Route path='/folder/:folderId'
-                        render={(props) => <NoteStorageFolder {...props}
-                            folders={this.state.folders}
-                            notes={this.state.notes}
-                            deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
-                    />
-                    <Route path='/note/:noteId'
-                        render={(props) => <NoteStorageNote {...props}
-                            notes={this.state.notes}
-                            deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
-                    />
-                </div>
+                <NotefulContext.Provider
+                    value={contextValue}>
+                    <div className='flex-one'>
+                        <Route exact path='/'
+                            render={() => <SideBar folders={this.state.folders} />}
+                        />
+                        <Route path='/folder/:folderId'
+                            render={() => <SideBarFolder folders={this.state.folders} />}
+                        />
+                        <Route path='/note/:noteId'
+                            render={(props) => <SideBarNote {...props}
+                                notes={this.state.notes}
+                                folders={this.state.folders} />}
+                        />
+                    </div>
+                    <div className='flex-three'>
+                        <Route exact path='/'
+                            render={() => <NoteStorage notes={this.state.notes}
+                                deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
+                        />
+                        <Route path='/folder/:folderId'
+                            render={(props) => <NoteStorageFolder {...props}
+                                folders={this.state.folders}
+                                notes={this.state.notes}
+                                deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
+                        />
+                        <Route path='/note/:noteId'
+                            render={(props) => <NoteStorageNote {...props}
+                                notes={this.state.notes}
+                                deleteButtonClick={(itemId) => this.handleDeleteItem(itemId)} />}
+                        />
+                    </div>
+                </NotefulContext.Provider>
             </div>
         )
     }
