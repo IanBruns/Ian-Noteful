ec9b18e0facf14b84d73636c16928731e33ef0e6
Ian Bruns <ian@rbruns.com>
2020-12-10 12:01:47 -0700

    adding AddNote component

---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AddNote extends React.Component {
+    render() {
+        return (
+            <div>
+                Adding that note here
+            </div>
+        )
+    }
+}
+
+export default AddNote;
