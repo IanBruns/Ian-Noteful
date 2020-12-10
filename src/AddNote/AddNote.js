d314af27db09a33031e7e074a357395f7e68e002
Ian Bruns <ian@rbruns.com>
2020-12-10 12:45:24 -0700

    state changes to text fields up and running

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,10 +1,82 @@
 import React from 'react'
+import NotefulContext from '../NotefulContext';
 
 class AddNote extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            noteName: '',
+            noteContent: '',
+            targetFolder: '',
+        }
+    }
+    static contextType = NotefulContext;
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        console.log('click')
+    }
+
+    updateNoteName(newNoteName) {
+        this.setState({
+            noteName: newNoteName
+        })
+    }
+
+    updateNoteContent(newNoteContent) {
+        this.setState({
+            noteContent: newNoteContent
+        })
+    }
+
     render() {
+        const { folders } = this.context;
+
+        const selectOptions = folders.map((folder, i) => {
+            return (
+                <option key={i} value={folder.id}>{folder.name}</option>
+            )
+        })
+
         return (
-            <div>
-                Adding that note here
+            <div className='AddNote'>
+                <form className='addNoteForm' onSubmit={e => this.handleSubmit(e)}>
+                    <fieldset>
+                        <legend>Add a New Note!</legend>
+
+                        <label htmlFor='noteName'>Note Name:</label>
+                        <br></br>
+                        <input type='text' name='noteName' id='noteName'
+                            value={this.state.noteName}
+                            onChange={e => this.updateNoteName(e.target.value)} />
+
+                        <br></br>
+
+                        <label htmlFor='noteContent'>Note Content:</label>
+                        <br></br>
+                        <textarea id="noteContent" name="noteContent" rows="4" cols="50"
+                            value={this.state.noteContent}
+                            onChange={e => this.updateNoteContent(e.target.value)} />
+
+                        <br></br>
+
+                        <label htmlFor='targetFolder'>Add to Which Folder?</label>
+                        <br></br>
+                        <select name='targetFolder' id='targetFolder'>
+                            {selectOptions}
+                        </select>
+
+                        <br></br>
+
+                        <button type='submit'>
+                            Create New Folder!
+                        </button>
+                    </fieldset>
+                </form>
+                <button onClick={() => this.props.history.goBack()}>
+                    Cancel
+                </button>
             </div>
         )
     }
